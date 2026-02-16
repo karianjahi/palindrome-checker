6afffd167fd3ba2cb5a01ba2e9d7ee61132f1af0
karianjahi <nkarianjahi@gmail.com>
2026-02-16 14:10:54 +0100

    Create a function that checkes for a palindrome and implement it on check button

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 const textInput = document.getElementById("text-input");
 const checkBtn = document.getElementById("check-btn");
+const result = document.getElementById("result");
+
+const palindromChecker = (possiblePalindrome) => {
+    let splits = [];
+    for (let character of possiblePalindrome) if (/[a-zA-Z0-9]/.test(character)) splits.push(character);    
+    const alphaNum = splits.join("").toLowerCase();
+    const alphaNumReverse = (alphaNum.split("").reverse().join(""));
+    if (alphaNum === alphaNumReverse) 
+        return `${possiblePalindrome} is a palindrome`;
+    return `${possiblePalindrome} is not a palindrome`;
+};
+
+
+
+// tests
+// console.log(palindromChecker("Sunilam al inus"));
+// console.log(palindromChecker("Japheth Wahome can't even blame anyone for what has happened to him"));
 
 checkBtn.addEventListener("click", ()=> {
     if (textInput.value.length === 0)  alert("Please input a value");
+    result.textContent = palindromChecker(textInput.value);
+    
 });
