9095ebe34d5a22f003067442998feb90f5cd8fa6
karianjahi <nkarianjahi@gmail.com>
2026-02-16 13:18:35 +0100

    Create a new folder and put the old fronted files there

---
+++ alternative_files/script.js
@@ -0,0 +1,5 @@
+const possiblePalindrome = document.getElementById(possible-palindrome);
+
+possiblePalindrome.addEventListener("click", ()=> {
+    possiblePalindrome.style.borderColor = "black";
+});
